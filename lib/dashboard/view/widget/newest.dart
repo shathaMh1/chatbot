890aed462d817a61d8b9f99f7ff9e8ae7373458d
Haneen Alrewaished <haneenalrewaished@Haneens-MacBook-Pro.local>
2022-12-14 14:13:24 +0300

    dashboard file with authentication

--- lib/dashboard/view/widget/newest.dart
+++ lib/dashboard/view/widget/newest.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../logic/controllers/dashboard_controller.dart';
-import 'package:badges/badges.dart';
 
 class NewestChat extends StatelessWidget {
   NewestChat({Key? key}) : super(key: key);
